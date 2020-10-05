e7a86502b683684088a861a0e52d978fe2cdfb23
darshan315 <darshankbr@gmail.com>
2020-10-05 04:56:14 +0200

    Updated notebook

--- mesh_study.sh
+++ mesh_study.sh
@@ -5,18 +5,17 @@ mesh_size=( 100 200 400 )
 	
 for i in "${mesh_size[@]}"
 do
-	mkdir -p test_cases/run/refinement_$i
 	cp -r test_cases/cylinder2D_base test_cases/run/refinement_$i
 	mkdir -p notebooks/plot_data/cases/case_$i
-	cd ./test_cases/run/refinement_$i/cylinder2D_base/
+	cd ./test_cases/run/refinement_$i/
 	echo -e "******************************************************************"
 	echo -e "\nMesh size = $i\n"
 	echo -e "******************************************************************"
 	./Allclean
 	./mesh bmcell $i
 	./solveronly
-	cd .. ; cd .. ; cd ..; cd ..
-	cp -r test_cases/run/refinement_$i/cylinder2D_base/system notebooks/plot_data/cases/case_$i
-	cp -r test_cases/run/refinement_$i/cylinder2D_base/postProcessing notebooks/plot_data/cases/case_$i
+	cd .. ; cd .. ; cd ..
+	cp -r test_cases/run/refinement_$i/system notebooks/plot_data/cases/case_$i
+	cp -r test_cases/run/refinement_$i/postProcessing notebooks/plot_data/cases/case_$i
 	
 done
