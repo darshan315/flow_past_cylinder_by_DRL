8cf551d119749967c2dc765d58534676bf6e6b7f
darshan315 <darshankbr@gmail.com>
2021-02-14 01:38:31 +0100

    cleaning dir and files

--- DRL_py/executing_jobs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for folder in $(ls -d ${PWD}/env/trajectories/trajectory_$1_$2/*); do
-  sbatch $folder/jobscript.sh
-done
--- DRL_py/link_to_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-ln -sfn of2006-py1.6-cpu.sif DRL_py/of2006-py1.6-cpu.sif
-
-cd DRL_py
