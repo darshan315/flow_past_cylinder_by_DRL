ed0c1aaf50909632e4a7e1517003bd873f376b11
darshan315 <darshankbr@gmail.com>
2021-02-16 03:57:52 +0100

    clearing bash dependancy #24

--- DRL_py/env_local.py
+++ DRL_py/env_local.py
@@ -2,20 +2,28 @@ import numpy as np
 import _thread
 import queue
 import subprocess
+import os
 
 
 class env:
-    def __init__(self, n_worker, buffer_size, base_setup_end_time, trajectory_end_time, base_case_setup, buffer):
+    def __init__(self, n_worker, buffer_size, base_setup_end_time, trajectory_end_time,
+                 base_case_setup, buffer, sample):
         self.n_worker = n_worker
         self.buffer_size = buffer_size
         self.base_setup_end_time = base_setup_end_time
         self.trajectory_end_time = trajectory_end_time
         self.base_case_setup = base_case_setup
         self.buffer = buffer
+        self.sample = sample
 
     def initial_setup(self):
         print("\n Forming base case...\n")
-        p = subprocess.run(['sh', 'env_local_initial.sh', str(self.base_setup_end_time)], check=True)
+        path = "./env/initial_setup"
+        os.makedirs(path, exist_ok=True)
+        os.system(f'cp -r ./env/base_case/* {path}/ &&'
+                  f'sed -i "/^startTime/ s/startTime.*/startTime       0;/g" {path}/system/controlDict &&'
+                  f'sed -i "/^endTime/ s/endTime.*/endTime         {self.base_setup_end_time};/g" {path}/system/controlDict &&'
+                  f'singularity run ../of2006-py1.6-cpu.sif ./Allrun {path}/')
         print("\n Initial setup is done... \n")
 
     def read_data(self, trajectory, buffer):
@@ -35,6 +43,19 @@ class env:
         finally:
             que.put((job_name, proc.returncode))
 
+    def run_trajectory(self, buffer_counter, proc, results):
+        traj_path = f"./env/sample_{self.sample}/trajectory_{buffer_counter}"
+        print(f"\n starting trajectory : {buffer_counter} \n")
+        os.makedirs(traj_path, exist_ok=True)
+        os.popen(f'cp -r ./env/initial_setup/* {traj_path}/ && '
+                 f'sed -i "/^startFrom/ s/startFrom.*/startFrom       latestTime;/g" {traj_path}/system/controlDict &&'
+                 f'sed -i "/^endTime/ s/endTime.*/endTime         {self.trajectory_end_time};/g" {traj_path}/system/controlDict &&'
+                 f'rm {traj_path}/log*')
+        proc[buffer_counter] = subprocess.Popen(
+            ['singularity', 'run', '../of2006-py1.6-cpu.sif', './sim_processing', f'{traj_path}/'])
+        _thread.start_new_thread(self.process_waiter,
+                                 (proc[buffer_counter], f"trajectory_{buffer_counter}", results))
+
     def fill_buffer(self):
         buffer_counter = 0
         proc = []
@@ -49,11 +70,7 @@ class env:
             self.initial_setup()
 
         for n in np.arange(self.n_worker):
-            print("\n starting trajectory : {} \n".format(buffer_counter))
-            proc[buffer_counter] = subprocess.Popen(
-                ['sh', 'start_traj_local.sh', str(buffer_counter), str(trajectory_end_time)])
-            _thread.start_new_thread(self.process_waiter,
-                                     (proc[buffer_counter], "trajectory_{}".format(buffer_counter), results))
+            self.run_trajectory(buffer_counter, proc, results)
             process_count += 1
             buffer_counter += 1
 
@@ -63,11 +80,7 @@ class env:
             if self.buffer_size > buffer_counter:
                 self.read_data(job_name, self.buffer)
                 self.clean_trajectory(job_name)
-                print("\n starting trajectory : {} \n".format(buffer_counter))
-                proc[buffer_counter] = subprocess.Popen(
-                    ['sh', 'start_traj_local.sh', str(buffer_counter), str(trajectory_end_time)])
-                _thread.start_new_thread(self.process_waiter,
-                                         (proc[buffer_counter], "trajectory_{}".format(buffer_counter), results))
+                self.run_trajectory(buffer_counter, proc, results)
                 process_count += 1
                 buffer_counter += 1
             process_count -= 1
@@ -78,7 +91,9 @@ if __name__ == "__main__":
     buffer_size = 4
     base_setup_end_time = 0.2
     trajectory_end_time = 0.4
-    base_case_setup = True
+    base_case_setup = False
     buffer = []
-    env = env(n_worker, buffer_size, base_setup_end_time, trajectory_end_time, base_case_setup, buffer)
+    sample = 1
+    env = env(n_worker, buffer_size, base_setup_end_time, trajectory_end_time, base_case_setup, buffer, sample)
+    #  fór loop for multiple sample : var : sample
     env.fill_buffer()
