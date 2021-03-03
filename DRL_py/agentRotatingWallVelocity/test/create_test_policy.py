f454e24f29d31fbac78a6713a1dfb72e406f70e1
Andre Weiner <andre.weiner@yahoo.de>
2021-03-03 09:37:28 +0100

    Add boundary condition for RL cylinder setup.

---
+++ DRL_py/agentRotatingWallVelocity/test/create_test_policy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import torch
+
+torch.set_default_tensor_type(torch.DoubleTensor)
+
+class TestPolicy(torch.nn.Module):
+    def __init__(self, n_inputs, neurons_per_layer):
+        super(TestPolicy, self).__init__()
+        self.linear_0 = torch.nn.Linear(n_inputs, neurons_per_layer)
+        self.linear_1 = torch.nn.Linear(neurons_per_layer, neurons_per_layer)
+        self.linear_2 = torch.nn.Linear(neurons_per_layer, 2)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.linear_0(x))
+        x = torch.nn.functional.relu(self.linear_1(x))
+        return self.linear_2(x)
+
+
+
+scripted = torch.jit.script(TestPolicy(54, 20))
+scripted.save("policy.pt")
+policy = torch.jit.load("policy.pt")
+example = torch.ones((2, 54))
+output = policy(example)
+print(output)
---
+++ DRL_py/agentRotatingWallVelocity/test/read_example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# read trajectory data
+## number of cell faces forming the cylinder patch
+n_faces = 54
+names = ["t", "omega", "log_p", "entropy", "theta_sum", "dt_theta_sum"]
+p_names = ["p{:d}".format(i) for i in range(n_faces)]
+
+trajectory = pd.read_csv("trajectory.csv", sep=",", names=names+p_names, header=0)
+
+## example 1: get pressure values as numpy array; .values converts dataframe to numpy array
+## the p array should have the shape [n_time_steps, n_faces]
+p = trajectory[p_names].values
+print("Shape of p:", p.shape)
+
+## example 2: get logarithmic probabilites as numpy array
+## the log_p array should have the shape [n_time_steps]
+log_p = trajectory.log_p.values
+print("Shape of log_p: ", log_p.shape)
+
+# read force coefficients
+names_coeffs = ["col{:d}".format(i) for i in range(13)]
+## we only want time, drag, and lift
+names_coeffs[0] = "t"
+names_coeffs[1] = "c_d"
+names_coeffs[3] = "c_l"
+keep = ["t", "c_d", "c_l"]
+
+file_path = "postProcessing/forces/0/coefficient.dat"
+coeffs = pd.read_csv(file_path, sep="\t", names=names_coeffs, usecols=keep, comment="#")
+## for some reason the function object's writeControls do not work properly; therefore,
+## we pick only every nth row from the dataframe; this number should be the same as for
+## the trajectory (specified in the boundary condition)
+pick_every = 20
+coeffs = coeffs[coeffs.index % pick_every == 0]
+print("Shape of force coefficients: ", coeffs.shape)
+
+## check for some examples if the times are equivalent
+print(trajectory.t.values[0], trajectory.t.values[5], trajectory.t.values[-1])
+print(coeffs.t.values[0], coeffs.t.values[5], coeffs.t.values[-1])
